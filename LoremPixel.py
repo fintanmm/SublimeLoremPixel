6637f12fdcb0c96c940c83fa9d437c886df662f1
Fintan MacMahon <fintan.macmahon@gmail.com>
2012-05-18 06:35:06 +0100

    Added support for auto complete

--- LoremPixel.py
+++ LoremPixel.py
@@ -1,15 +1,29 @@
 import sublime
 import sublime_plugin
 
-# TODO: Display command
-# TODO: Display list of sizes
 # TODO: Custom sizes
-# TODO: Categories eg sports, animals
-# TODO: Autocomplete
+# TODO: Categories
+# TODO: Grey photos
+# TODO: Dummy text
+# TODO: Validate custom_sizes
+# TODO: Disable categories
+# TODO: Support for less and sass
 
-# http://lorempixel.com/
 
+class LoremPixel(sublime_plugin.EventListener):
+    def on_query_completions(self, view, prefix, locations):
+        syntax = view.settings().get('syntax')
+        settings = sublime.load_settings("LoremPixel.sublime-settings")
+        pixel_url = 'http://lorempixel.com/'
+        sizes = settings.get("custom_sizes")
+        categories = settings.get("categories")
+        sel = view.sel()[0].a
+        attr = view.substr(sublime.Region(sel - 5, sel))
+        urls = []
 
-class LoremPixelCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        for x in (u'html', u'css', u'less', u'sass'):
+            if syntax.lower().count(x):
+                if attr.count('src=') or attr.count('url('):
+                    urls = [(pixel_url + s, pixel_url + s) for s in sizes]
+            return urls
+        return
---
+++ README.md
@@ -0,0 +1,15 @@
+SublimeLoremPixel
+=================
+
+Inserts url's for lorempixel.com into html and css files.
+
+Todo:
+------------------------
+* Custom sizes
+* Categories
+* Grey photos
+* Dummy text
+* Validate custom_sizes
+* Support for less and sass
+
+Not affiliated with lorempixel.com, you mileage may vary.
