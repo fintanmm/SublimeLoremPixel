2170799c7ee225fb1b88f19e17dd7d3038eec04b
outer-edge <fintan.macmahon@gmail.com>
2012-05-22 21:18:10 +0100

    Added support for categories and also the ability to disable them

--- LoremPixel.py
+++ LoremPixel.py
@@ -1,34 +1,44 @@
 import sublime
 import sublime_plugin
+import re
 
-# TODO: Custom sizes
-# TODO: Categories
 # TODO: Grey photos
 # TODO: Dummy text
-# TODO: Validate custom_sizes
-# TODO: Disable categories
 # TODO: Support for less and sass
+# FIXME: Don't show categories when the sizes are not present
+
+lp = re.compile('lorempixel\.com(\/)|(\/g/)\[\d]*\/[\d]*/')
 
 
 class LoremPixel(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
         syntax = view.settings().get('syntax')
         settings = sublime.load_settings("LoremPixel.sublime-settings")
-        pixel_url = 'http://lorempixel.com/'
         sizes = settings.get("custom_sizes")
-        categories = settings.get("categories")
-        sel = view.sel()[0].a
-        attr = view.substr(sublime.Region(sel - 5, sel))
+        categories = []
+
+        if settings.get("include_categories"):
+            categories = settings.get("categories")
+
+        def posistion(pos):  # TODO: Need a better way to do this
+            sel = view.sel()[0].a
+            return view.substr(sublime.Region(sel - pos, sel))
+
+        pixel_url = 'http://lorempixel.com/'
+        defaults = [(pixel_url, pixel_url)]
         urls = []
 
+        if settings.get("include_grey"):
+            defaults.append((pixel_url + 'g/', pixel_url + 'g/'))
+
+        attr = posistion(5)
+        lp_attr = posistion(50)
+
         for x in (u'html', u'css', u'less', u'sass'):
-            if syntax.lower().count(x):
-                if attr.count('src=') or attr.count('url('):
-                    urls = [(pixel_url + s, pixel_url + s) for s in sizes]
+            if syntax.lower().find(x):
+                if attr.find('src=') != -1 or attr.find('url(') != -1:
+                    urls = defaults + [(pixel_url + s, pixel_url + s) for s in sizes]
+                if lp.search(lp_attr):
+                    urls = [(c, c) for c in categories]
             return urls
         return
-
-
-class LoremPixelCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
--- README.md
+++ README.md
@@ -5,11 +5,8 @@ Inserts url's for lorempixel.com into html and css files.
 
 Todo:
 ------------------------
-* Custom sizes
-* Categories
 * Grey photos
 * Dummy text
-* Validate custom_sizes
 * Support for less and sass
 
 Not affiliated with lorempixel.com, you mileage may vary.
